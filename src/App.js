dfe9fcbb282012aedbc87f939a170384da2f6222
cold-pac <erol.a.cimen@gmail.com>
2020-04-29 01:36:56 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const keys = "qweasdzxc";
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render () {
+    return  (
+      <div id = "drum-machine">
+        <div id = "pads">
+          {keys.toUpperCase().split("").map((elem) => <div className = "drum-pad" id = {elem}>{elem}</div>)}
+        </div>
+        <div id = "buttons">
+          <div id = "power"><span>Power</span></div>
+          <div id = "display"><span>Display:</span></div>
+        </div>
+      </div>
+      );
+  }
+}
+
+window.onkeypress = (event) => {
+  if (keys.split("").indexOf(event.key.toLowerCase()) !== -1) {
+    document.getElementById(event.key.toUpperCase()).click();
+  }
 }
 
 export default App;
