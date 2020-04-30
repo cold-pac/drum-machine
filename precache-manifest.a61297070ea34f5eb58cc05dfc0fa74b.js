777276ead98b3f20c08ac536a3016e7013ed1487
cold-pac <erol.a.cimen@gmail.com>
2020-04-30 20:53:21 +1000

    Updates

---
+++ precache-manifest.a61297070ea34f5eb58cc05dfc0fa74b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885d3fefce75701bbbe0fbc479d3a705",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "955f407952c629cb4f53",
+    "url": "/drum-machine/static/css/main.3c03960a.chunk.css"
+  },
+  {
+    "revision": "8185e857882c082cbfda",
+    "url": "/drum-machine/static/js/2.7b254bd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.7b254bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955f407952c629cb4f53",
+    "url": "/drum-machine/static/js/main.31f5ba6e.chunk.js"
+  },
+  {
+    "revision": "e426264f021e369657ac",
+    "url": "/drum-machine/static/js/runtime-main.5bfd1e01.js"
+  }
+]);
